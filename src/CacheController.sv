7956e9c09eef2ae716ba85bc462a3de4aa707017
Ryken Thompson <rykenthompson@icloud.com>
2024-05-23 16:05:59 -0700

    cache progress

--- src/CacheController.sv
+++ src/CacheController.sv
@@ -29,7 +29,22 @@
 
 module CacheController(
     input CLK,
-    input RST
+    input RST,
+    input mem_read,
+    input mem_write,
+    input hit,
+    input lru_valid,
+    input lru_dirty,
+    input cl_busy,
+    output update_lru,
+    output update_tag,
+    output mem_valid,
+    output set_dirty,
+    output clear_dirty,
+    output set_valid,
+    output clear_valid,
+    output cl_read,
+    output cl_write
     );
 
     typedef enum {IDLE, HIT_CHECK, MISS, MEM_READ, REFILL} state;
@@ -45,8 +60,83 @@ module CacheController(
     end
 
     always_comb begin
+        update_lru = 0;
+        update_tag = 0;
+        mem_valid = 0;
+        set_dirty = 0;
+        clear_dirty = 0;
+        set_valid = 0;
+        clear_valid = 0;
+        cl_read = 0;
+        cl_write = 0;
         case (ps)
-
+            IDLE: begin
+                //Wait for mem read or mem write (constantly reading the tag/index arrays checking for hits)
+                if (read | write) begin
+                    ns = HIT_CHECK;
+                end
+                else begin
+                    ns = IDLE;
+                end
+            end
+            HIT_CHECK: begin
+                //Check if there was a hit on mem read or write
+                if (hit & mem_read) begin
+                    update_lru = 1'b1;
+                    mem_valid = 1'b1;
+                    ns = IDLE;
+                end
+                else if (hit & mem_write) begin
+                    update_lru = 1'b1;
+                    mem_valid = 1'b1;
+                    set_dirty = 1'b1;
+                    ns = IDLE;
+                end
+                else begin
+                    ns = MISS;
+                end
+            end
+            MISS: begin
+                if (lru_valid & lru_dirty) begin
+                    //If cache miss and data was valid and dirty writeback to memory
+                    cl_write = 1'b1;
+                    clear_valid = 1'b1;
+                    clear_dirty = 1'b1;
+                    ns = MEM_READ;
+                end
+                else if (~lru_valid | (lru_valid & ~lru_dirty)) begin
+                    //No writeback neeeded start read into cache
+                    cl_read = 1'b1;
+                    ns = REFILL;
+                end
+                else begin
+                    //Shouldn't get here
+                    ns = IDLE;
+                end
+            end
+            MEM_READ: begin
+                if (cl_busy) begin
+                    ns = MEM_READ;
+                end
+                else begin
+                    cl_read = 1'b1;
+                    ns = REFILL;
+                end
+            end
+            REFILL: begin
+                if (cl_busy) begin
+                    ns = REFILL;
+                end
+                else begin
+                    update_tag = 1'b1;
+                    set_valid = 1'b1;
+                    clear_dirty = 1'b1;
+                    ns = HIT_CHECK;
+                end
+            end
+            default: begin 
+                ns = IDLE;
+            end
         endcase
     end
 
--- src/cache.sv
+++ src/cache.sv
@@ -4,96 +4,129 @@ module cache (
 	input addr,
 	input [31:0] data_in,
 	input [255:0] cacheline_in,
+	input update_lru,
+	input update_tag,
+	input set_dirty,
+    input clear_dirty,
+    input set_valid,
+    input clear_valid,
 	output hit,
+	output lru_dirty,
+	output lru_valid,
 	output logic [31:0] data_out,
 	output logic [255:0] cacheline_out
 );
 
-	typedef struct packed {
-		logic valid [0:7];
-		logic dirty [0:7];
-		logic [25:0] tag [0:7];
-		logic [255:0] data [0:7];
-	} way_t;
+	/*------------------------------------------------------------------------------
+	-- CACHE ARRAYS
+	------------------------------------------------------------------------------*/
 
-	way_t way1;
-	way_t way2;
+	logic c_valid [0:7][0:1];
+	logic c_dirty [0:7][0:1];
+	logic [25:0] c_tag [0:7][0:1];
+	logic [255:0] c_data [0:7][0:1];
 
-	logic [25:0] tag;
+	logic [25:0] i_tag;
 	logic [2:0] index;
 	logic [2:0] offset;
 
-	logic lru;
-	logic lru_dirty;
-	logic lru_valid;
+	logic lru [0:7];
+
+	logic w1_hit, w2_hit;
+
+	/*------------------------------------------------------------------------------
+	--  Cache Logic
+	------------------------------------------------------------------------------*/
 
 	assign tag = addr[31:6];
 	assign index = addr[5:3];
 	assign offset = addr[2:0];
 
-	logic way1_hit, way2_hit;
+	assign lru_dirty = c_dirty[index][lru[index]];
+	assign lru_valid = c_valid[index][lru[index]];
 
-	assign hit = (way1.hit | way2.hit);
+	assign hit = (w1_hit | w2_hit);
 
 	//Hit logic
 	always_ff @(posedge CLK) begin
 		if(RST) begin
-			way1_hit <= 0;
-			way2_hit <= 0;
+			w1_hit <= 0;
+			w2_hit <= 0;
 		end else begin
-			way1_hit <= ((way1.tag[index] == tag) & way1.valid[index]);
-			way2_hit <= ((way2.tag[index] == tag) & way2.valid[index]);
+			w1_hit <= ((c_tag[index][0] == tag) & c_valid[index][0]);
+			w2_hit <= ((c_tag[index][1] == tag) & c_valid[index][1]);
 		end
 	end
 
 	//Psuedo LRU
 	always_ff @(posedge CLK) begin
 		if(RST) begin
-			lru <= 1'b0;
-
-		end
-		else if (read | write) begin
-			if (way1_hit) begin
-				lru <= 1'b0;
-				lru_valid <= way1.valid[index];
-				lru_dirty <= way1.dirty[index];
-			end
-			else if (way2_hit) begin
-				lru <= 1'b1;
-				lru_valid <= way2.valid[index];
-				lru_dirty <= way2.dirty[index];
-			end
+			lru <= 0;
+		end else if (update_lru) begin
+			lru[index] <= w2_hit;
 		end
 	end
 
+	//Valid Bit Array
+	always_ff @(posedge CLK) begin
+		if(RST) begin
+			c_valid <= 0;
+		end else if (set_valid) begin
+			c_valid[index][lru[index]] <= 1'b1;
+		end
+		else if (clear_valid) begin
+			c_valid[index][lru[index]] <= 1'b0;
+		end
+	end
+
+	//Dirty Bit Array
+	always_ff @(posedge CLK) begin
+		if(RST) begin
+			c_dirty <= 0;
+		end else if (set_dirty) begin
+			c_dirty[index][lru[index]] <= 1'b1;
+		end
+		else if (clear_dirty) begin
+			c_dirty[index][lru[index]] <= 1'b0;
+		end
+	end
+
+	//Tag Array
+	always_ff @(posedge CLK) begin
+		if(RST) begin
+			c_tag <= 0;
+		end else if (update_tag) begin
+			c_tag[index][lru[index]] <= tag;
+		end
+	end
 
 	//Async Reeeads
 	always_comb begin
-		if (way1_hit) begin 
-			cacheline_out = way1.data[index];
+		if (w1_hit) begin 
+			cacheline_out = c_data[index][0];
 			case (offset)
-				0: data = way1.data[index][31:0];
-				1: data = way1.data[index][63:32];
-				2: data = way1.data[index][95:64];
-				3: data = way1.data[index][127:96];
-				4: data = way1.data[index][159:128];
-				5: data = way1.data[index][191:160];
-				6: data = way1.data[index][223:192];
-				7: data = way1.data[index][255:224];
+				0: data = c_data[index][0][31:0];
+				1: data = c_data[index][0][63:32];
+				2: data = c_data[index][0][95:64];
+				3: data = c_data[index][0][127:96];
+				4: data = c_data[index][0][159:128];
+				5: data = c_data[index][0][191:160];
+				6: data = c_data[index][0][223:192];
+				7: data = c_data[index][0][255:224];
 				default: data = 32'hdeadbeef;
 			endcase
 		end
 		else if (way2_hit) begin
-			cacheline_out = way2.data[index];
+			cacheline_out = c_data[index][1];
 			case (offset)
-				0: data = way2.data[index][31:0];
-				1: data = way2.data[index][63:32];
-				2: data = way2.data[index][95:64];
-				3: data = way2.data[index][127:96];
-				4: data = way2.data[index][159:128];
-				5: data = way2.data[index][191:160];
-				6: data = way2.data[index][223:192];
-				7: data = way2.data[index][255:224];
+				0: data = c_data[index][1][31:0];
+				1: data = c_data[index][1][63:32];
+				2: data = c_data[index][1][95:64];
+				3: data = c_data[index][1][127:96];
+				4: data = c_data[index][1][159:128];
+				5: data = c_data[index][1][191:160];
+				6: data = c_data[index][1][223:192];
+				7: data = c_data[index][1][255:224];
 				default: data = 32'hdeadbeef;
 			endcase
 		end
@@ -110,37 +143,36 @@ module cache (
 			way1 <= 0;
 			way2 <= 0;
 		end
-		else if (cacheline_write) begin
-			
-		end
+
+		/*
 		else if (way1_hit & write) begin
-			way1.dirty[index] <= 1'b1;
+			w1_dirty[index] <= 1'b1;
 			case (offset)
-				0: way1.data[index][31:0] <= data_in;
-				1: way1.data[index][63:32] <= data_in;
-				2: way1.data[index][95:64] <= data_in;
-				3: way1.data[index][127:96] <= data_in;
-				4: way1.data[index][159:128] <= data_in;
-				5: way1.data[index][191:160] <= data_in;
-				6: way1.data[index][223:192] <= data_in;
-				7: way1.data[index][255:224] <= data_in;
+				0: w1_data[index][31:0] <= data_in;
+				1: w1_data[index][63:32] <= data_in;
+				2: w1_data[index][95:64] <= data_in;
+				3: w1_data[index][127:96] <= data_in;
+				4: w1_data[index][159:128] <= data_in;
+				5: w1_data[index][191:160] <= data_in;
+				6: w1_data[index][223:192] <= data_in;
+				7: w1_data[index][255:224] <= data_in;
 				default: data <= 32'hdeadbeef;
 			endcase
 		end
 		else if (way2_hit & write) begin
-			way2.dirty[index] <= 1'b1;
+			w2_dirty[index] <= 1'b1;
 			case (offset)
-				0: way2.data[index][31:0] <= data_in;
-				1: way2.data[index][63:32] <= data_in;
-				2: way2.data[index][95:64] <= data_in;
-				3: way2.data[index][127:96] <= data_in;
-				4: way2.data[index][159:128] <= data_in;
-				5: way2.data[index][191:160] <= data_in;
-				6: way2.data[index][223:192] <= data_in;
-				7: way2.data[index][255:224] <= data_in;
-				default: way2.data <= 256'hdeadbeef;
+				0: w2_data[index][31:0] <= data_in;
+				1: w2_data[index][63:32] <= data_in;
+				2: w2_data[index][95:64] <= data_in;
+				3: w2_data[index][127:96] <= data_in;
+				4: w2_data[index][159:128] <= data_in;
+				5: w2_data[index][191:160] <= data_in;
+				6: w2_data[index][223:192] <= data_in;
+				7: w2_data[index][255:224] <= data_in;
+				default: w2_data <= 256'hdeadbeef;
 			endcase
-		end
+		end*/
 	end
 
 endmodule : cache
