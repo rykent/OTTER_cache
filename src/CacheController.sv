d5f9adda548f3a1969fd7e308f9630e70b197b5f
Ryken Thompson <rykenthompson@icloud.com>
2024-05-23 08:04:31 -0700

    ahhh

--- src/CacheController.sv
+++ src/CacheController.sv
@@ -32,7 +32,7 @@ module CacheController(
     input RST
     );
 
-    typedef enum {IDLE, WR, RE, LRU_DIRTY, MEM_READ, CACHE_EDIT} state;
+    typedef enum {IDLE, HIT_CHECK, MISS, MEM_READ, REFILL} state;
 
     state ps,ns;
 
@@ -46,38 +46,7 @@ module CacheController(
 
     always_comb begin
         case (ps)
-            //Wait for cache reads or writes
-            IDLE: begin
-                if (mem_read) ns = RE;
-                else if (mem_write) ns = WR;
-                else ns = IDLE;
-            end
-            WR: begin
-                //Cache Hit go back to IDLE
-                if (hit & (~mem_write | ~mem_read)) begin
-                    mem_resp = 1'b1;
-                    ns = IDLE;
-                end
-                else if (hit & mem_write) begin
-                    mem_resp = 1'b1;
-                    ns = WR;
-                end
-                else if (hit & mem_read) begin
-                    mem_resp = 1'b1;
-                    ns = RE;
-                end
-                else if (lru_dirty) begin
-                    mem_resp = 1'b0;
-                    ns = LRU_DIRTY;
-                end
-                else begin
-                    mem_resp = 1'b0;
-                    ns = MEM_READ;
-                end
-            end
-            RE: begin
-                
-            end
+
         endcase
     end
 
--- src/cache.sv
+++ src/cache.sv
@@ -3,9 +3,10 @@ module cache (
 	input RST,
 	input addr,
 	input [31:0] data_in,
+	input [255:0] cacheline_in,
 	output hit,
 	output logic [31:0] data_out,
-	output logic mem_valid
+	output logic [255:0] cacheline_out
 );
 
 	typedef struct packed {
@@ -22,62 +23,123 @@ module cache (
 	logic [2:0] index;
 	logic [2:0] offset;
 
+	logic lru;
+	logic lru_dirty;
+	logic lru_valid;
+
 	assign tag = addr[31:6];
 	assign index = addr[5:3];
 	assign offset = addr[2:0];
 
-	/*logic [26:0] tag_way1 [0:7];
-	logic [26:0] tag_way2 [0:7];
-
-	logic dirty_way1 [0:7];
-	logic dirty_way2 [0:7];
-
-	logic valid_way1 [0:7];
-	logic valid_way2 [0:7];
-
-	logic [255:0] cache_data_way1 [0:7];
-	logic [255:0] cache_data_way2 [0:7];*/
-
 	logic way1_hit, way2_hit;
 
-	assign way1_hit = ((way1.tag == tag) & way1.valid[index]);
-	assign way2_hit = ((way2.tag == tag) & way2.valid[index]);
 	assign hit = (way1.hit | way2.hit);
 
+	//Hit logic
+	always_ff @(posedge CLK) begin
+		if(RST) begin
+			way1_hit <= 0;
+			way2_hit <= 0;
+		end else begin
+			way1_hit <= ((way1.tag[index] == tag) & way1.valid[index]);
+			way2_hit <= ((way2.tag[index] == tag) & way2.valid[index]);
+		end
+	end
+
+	//Psuedo LRU
+	always_ff @(posedge CLK) begin
+		if(RST) begin
+			lru <= 1'b0;
+
+		end
+		else if (read | write) begin
+			if (way1_hit) begin
+				lru <= 1'b0;
+				lru_valid <= way1.valid[index];
+				lru_dirty <= way1.dirty[index];
+			end
+			else if (way2_hit) begin
+				lru <= 1'b1;
+				lru_valid <= way2.valid[index];
+				lru_dirty <= way2.dirty[index];
+			end
+		end
+	end
+
+
+	//Async Reeeads
+	always_comb begin
+		if (way1_hit) begin 
+			cacheline_out = way1.data[index];
+			case (offset)
+				0: data = way1.data[index][31:0];
+				1: data = way1.data[index][63:32];
+				2: data = way1.data[index][95:64];
+				3: data = way1.data[index][127:96];
+				4: data = way1.data[index][159:128];
+				5: data = way1.data[index][191:160];
+				6: data = way1.data[index][223:192];
+				7: data = way1.data[index][255:224];
+				default: data = 32'hdeadbeef;
+			endcase
+		end
+		else if (way2_hit) begin
+			cacheline_out = way2.data[index];
+			case (offset)
+				0: data = way2.data[index][31:0];
+				1: data = way2.data[index][63:32];
+				2: data = way2.data[index][95:64];
+				3: data = way2.data[index][127:96];
+				4: data = way2.data[index][159:128];
+				5: data = way2.data[index][191:160];
+				6: data = way2.data[index][223:192];
+				7: data = way2.data[index][255:224];
+				default: data = 32'hdeadbeef;
+			endcase
+		end
+		else begin 
+			cacheline_out = 256'hdeadbeef;
+			data = 32'hdeadbeef;
+		end
+	end
+
+
+	//Write logic
 	always_ff @(posedge CLK) begin : proc_
 		if(RST) begin
 			way1 <= 0;
 			way2 <= 0;
-			data <= 0;
 		end
-		else if (way1_hit & read) begin
-			case (offset)
-				0: data <= way1.data[31:0];
-				1: data <= way1.data[63:32];
-				2: data <= way1.data[95:64];
-				3: data <= way1.data[127:96];
-				4: data <= way1.data[159:128];
-				5: data <= way1.data[191:160];
-				6: data <= way1.data[223:192];
-				7: data <= way1.data[255:224];
-				default: data <= 32'hdeadbeef;
-			endcase
-		end
-		else if (way2_hit & read) begin
-			case (offset)
-				0: data <= way2.data[31:0];
-				1: data <= way2.data[63:32];
-				2: data <= way2.data[95:64];
-				3: data <= way2.data[127:96];
-				4: data <= way2.data[159:128];
-				5: data <= way2.data[191:160];
-				6: data <= way2.data[223:192];
-				7: data <= way2.data[255:224];
-				default: data <= 32'hdeadbeef;
-			endcase
-		end
-		else if (hit & write) begin
+		else if (cacheline_write) begin
 			
+		end
+		else if (way1_hit & write) begin
+			way1.dirty[index] <= 1'b1;
+			case (offset)
+				0: way1.data[index][31:0] <= data_in;
+				1: way1.data[index][63:32] <= data_in;
+				2: way1.data[index][95:64] <= data_in;
+				3: way1.data[index][127:96] <= data_in;
+				4: way1.data[index][159:128] <= data_in;
+				5: way1.data[index][191:160] <= data_in;
+				6: way1.data[index][223:192] <= data_in;
+				7: way1.data[index][255:224] <= data_in;
+				default: data <= 32'hdeadbeef;
+			endcase
+		end
+		else if (way2_hit & write) begin
+			way2.dirty[index] <= 1'b1;
+			case (offset)
+				0: way2.data[index][31:0] <= data_in;
+				1: way2.data[index][63:32] <= data_in;
+				2: way2.data[index][95:64] <= data_in;
+				3: way2.data[index][127:96] <= data_in;
+				4: way2.data[index][159:128] <= data_in;
+				5: way2.data[index][191:160] <= data_in;
+				6: way2.data[index][223:192] <= data_in;
+				7: way2.data[index][255:224] <= data_in;
+				default: way2.data <= 256'hdeadbeef;
+			endcase
 		end
 	end
 
